22d0f334693ac7b8bd187eba51a7ca79b4df273f
Aexander Sanchez <alezsd@yahoo.com>
2020-07-31 18:24:51 +0200

    modified with slots validations and personalized error messages

--- pagar/actions.py
+++ pagar/actions.py
@@ -22,11 +22,19 @@ class PagarForm(FormAction):
          return "pagar_form"
      
      def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+       
         return  {
-           "referencia": [self.from_entity(entity="referencia"), self.from_text()],
-           "tarjeta": [self.from_text()],
-           "cvv": [self.from_text()],
-           "mmaa": [self.from_text()]
+           
+           
+           "referencia": [self.from_entity(entity="referencia", intent=["inform_referencia","pagar"]), self.from_text()],
+           "tarjeta": [self.from_entity(entity="tarjeta", intent=["inform_tarjeta", "pagar"]), self.from_text()],
+           "cvv": [self.from_entity(entity="cvv", intent=["inform_cvv","pagar"]), self.from_text()],
+           "mmaa": [self.from_entity(entity="mmaa", intent=["inform_mmaa","pagar"]), self.from_text()]
+
+           #"referencia": [self.from_entity(entity="referencia", intent=["inform_referencia"])],
+           #"tarjeta": [self.from_entity(entity="tarjeta", intent=["inform_tarjeta"])],
+           #"cvv": [self.from_entity(entity="cvv", intent=["inform_cvv"])],
+           #"mmaa": [self.from_entity(entity="mmaa", intent=["inform_mmaa"])]
        }
 
      @staticmethod
@@ -40,9 +48,17 @@ class PagarForm(FormAction):
      def validate_cvv(self, value: Text, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any] ) -> Dict[Text, Any]:
          if (int(value) > 99 and int(value)<1000):
              # validation succeeded, set the value of the "cvv" slot to value
-             return {"cvv": value}
+             print("Codigo cvv validado")
+
+             #slot_values= self.extract_other_slots(dispatcher, tracker, domain)
+             slot_values= tracker.current_slot_values()
+             #if ("referencia" in slot_values.items() and "tarjeta" in slot_values.items()):
+             if slot_values["requested_slot"] == 'cvv':
+                return {"cvv": value}
+             else:
+                return {"cvv": None}
          else:
-             dispatcher.utter_message(template="utter_wrong")
+             dispatcher.utter_message(template="utter_wrong_cvv")
              # validation failed, set this slot to None, meaning the
              # user will be asked for the slot again
              return {"cvv": None}
@@ -51,14 +67,25 @@ class PagarForm(FormAction):
          if  (len(value) == 12):
              return {"referencia": value}
          else:
-             dispatcher.utter_message(template="utter_wrong")
+             dispatcher.utter_message(template="utter_wrong_referencia")
+           
+             slot_values= self.extract_other_slots(dispatcher, tracker, domain)
+             #for slot, value in slot_values.items():
+                  #if (slot == "cvv" or slot == "mmaa"):
+                  #  print(slot)
+                   # SlotSet(slot_values[slot], None)
+
              return {"referencia": None}
 
      def validate_tarjeta(self, value: Text, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any] ) -> Dict[Text, Any]:
          if  (len(value) == 16):
-             return {"tarjeta": value}
+             slot_values= tracker.current_slot_values()
+             if  slot_values["requested_slot"] == "tarjeta":
+                 return {"tarjeta": value}
+             else:
+                 return {"tarjeta": None}
          else:
-             dispatcher.utter_message(template="utter_wrong")
+             dispatcher.utter_message(template="utter_wrong_tarjeta")
              return {"tarjeta": None}
 
      @staticmethod
@@ -71,7 +98,11 @@ class PagarForm(FormAction):
 
      def validate_mmaa(self, value: Text, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any] ) -> Dict[Text, Any]:
          if  (len(value) == 4 and value[0:2] in self.meses_db() and value[2:4] in self.años_db()):
-             return {"mmaa": value}
+             slot_values= tracker.current_slot_values()
+             if slot_values["requested_slot"]=="mmaa":
+               return {"mmaa": value}
+             else: 
+               return {"mmaa": None}
          else:
-              dispatcher.utter_message(template="utter_wrong")
+              dispatcher.utter_message(template="utter_wrong_mmaa")
               return {"mmaa": None}
